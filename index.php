aec60feeafc7f50f03d691951602dc9a90f51358
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-06-09 01:29:47 +1200

    Created intitial styling for navigation bar

--- index.php
+++ index.php
@@ -2,10 +2,43 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale-1">
     <title>M-Dev-Store</title>
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+
 </head>
 <body>
+<div id="top">
+ <div class="container">
+ <div class="col-md-6 offer">
+     <a href="" class="btn btn-success btn-sn">Welcome</a>
+     <a href="checkout.php">4 Items In Your Cart | Total Price: $300</a>
+ </div>
+ <div class="col-md-6">
+     <ul class="menu">
+         <li>
+             <a href="customer_register.php">Register</a>
+         </li>
+         <li>
+             <a href="checkout.php">My Account</a>
+         </li>
+         <li>
+             <a href="cart.php">Go to Cart</a>
+         </li>
+         <li>
+             <a href="checkout.php">Login</a>
+         </li>
+     </ul>
+ </div>
+</div>
+
+</div>
+
+
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
     
 </body>
 </html>
