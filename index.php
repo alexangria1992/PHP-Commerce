9d4aab1fa671fac7b2f182a4961d8e695f488721
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-06-09 02:45:31 +1200

    Created Slider Homepage

--- index.php
+++ index.php
@@ -97,6 +97,42 @@
         </div>
     </div>
 </div>
+<div class="container" id="slider">
+    <div class="col-md-12">
+        <div class="carousel slide" id="myCarousel" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li class="active" data-target="#myCarousel" data-slide-to="0"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+
+            </ol>
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img src="admin_area/slides_images/slide-1.jpg" alt="Slider Image 1">
+                </div>
+                <div class="item">
+                    <img src="admin_area/slides_images/slide-2.jpg" alt="Slider Image 2">
+                </div>
+                <div class="item">
+                    <img src="admin_area/slides_images/slide-3.jpg" alt="Slider Image 3">
+                </div>
+                <div class="item">
+                    <img src="admin_area/slides_images/slide-4.jpg" alt="Slider Image 4">
+                </div>
+            </div>
+            <a href="#myCarousel" class="left carousel-control" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a href="#myCarousel" class="right carousel-control" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</div>
 
     <script src="js/jquery-331.min.js"></script>
     <script src="js/bootstrap-337.min.js"></script>
