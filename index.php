73e635b0936b8b6c5e86adaf4a53b161d3f63497
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-06-12 01:15:33 +1200

    Create Product Storefront

--- index.php
+++ index.php
@@ -168,6 +168,208 @@
   </div>
 </div>
 
+<div id="hot">
+    <div class="box">
+        <div class="container">
+            <div class="col-md-12">
+                <h2>
+                    Our Latest Products
+                </h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="content" class="container">
+    <div class="row">
+    <div class="col-sm-4 col-sm-6 single">
+            <div class="product">
+                <a href="details.php">
+                    <img class="img-responsive" src="admin_area/product_images/product-1.jpg" alt="Product 1">
+                </a>
+                <div class="text">
+                    <h3>
+                        <a href="details.php">
+                            M-Dev Woman T-Shirt
+                        </a>
+                    </h3>
+                    <p class="price">$30</p>
+                    <p class="button">
+                        <a href="details.php" class="btn btn-default">View Details</a>
+                        <a href="details.php" class="btn btn-primary">
+                            <i class="fa fa-shopping-cart">
+                                Add To Cart
+                            </i>
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 col-sm-6 single">
+            <div class="product">
+                <a href="details.php">
+                    <img class="img-responsive" src="admin_area/product_images/product-2.jpg" alt="Product 1">
+                </a>
+                <div class="text">
+                    <h3>
+                        <a href="details.php">
+                            M-Dev Man Logo T-Shirt 
+                        </a>
+                    </h3>
+                    <p class="price">$30</p>
+                    <p class="button">
+                        <a href="details.php" class="btn btn-default">View Details</a>
+                        <a href="details.php" class="btn btn-primary">
+                            <i class="fa fa-shopping-cart">
+                                Add To Cart
+                            </i>
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 col-sm-6 single">
+            <div class="product">
+                <a href="details.php">
+                    <img class="img-responsive" src="admin_area/product_images/product-1.jpg" alt="Product 1">
+                </a>
+                <div class="text">
+                    <h3>
+                        <a href="details.php">
+                            M-Dev Woman T-Shirt
+                        </a>
+                    </h3>
+                    <p class="price">$30</p>
+                    <p class="button">
+                        <a href="details.php" class="btn btn-default">View Details</a>
+                        <a href="details.php" class="btn btn-primary">
+                            <i class="fa fa-shopping-cart">
+                                Add To Cart
+                            </i>
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 col-sm-6 single">
+            <div class="product">
+                <a href="details.php">
+                    <img class="img-responsive" src="admin_area/product_images/product-2.jpg" alt="Product 1">
+                </a>
+                <div class="text">
+                    <h3>
+                        <a href="details.php">
+                            M-Dev Man Logo T-Shirt 
+                        </a>
+                    </h3>
+                    <p class="price">$30</p>
+                    <p class="button">
+                        <a href="details.php" class="btn btn-default">View Details</a>
+                        <a href="details.php" class="btn btn-primary">
+                            <i class="fa fa-shopping-cart">
+                                Add To Cart
+                            </i>
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-4 col-sm-6 single">
+            <div class="product">
+                <a href="details.php">
+                    <img class="img-responsive" src="admin_area/product_images/product-1.jpg" alt="Product 1">
+                </a>
+                <div class="text">
+                    <h3>
+                        <a href="details.php">
+                            M-Dev Woman T-Shirt
+                        </a>
+                    </h3>
+                    <p class="price">$30</p>
+                    <p class="button">
+                        <a href="details.php" class="btn btn-default">View Details</a>
+                        <a href="details.php" class="btn btn-primary">
+                            <i class="fa fa-shopping-cart">
+                                Add To Cart
+                            </i>
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 col-sm-6 single">
+            <div class="product">
+                <a href="details.php">
+                    <img class="img-responsive" src="admin_area/product_images/product-2.jpg" alt="Product 1">
+                </a>
+                <div class="text">
+                    <h3>
+                        <a href="details.php">
+                            M-Dev Man Logo T-Shirt 
+                        </a>
+                    </h3>
+                    <p class="price">$30</p>
+                    <p class="button">
+                        <a href="details.php" class="btn btn-default">View Details</a>
+                        <a href="details.php" class="btn btn-primary">
+                            <i class="fa fa-shopping-cart">
+                                Add To Cart
+                            </i>
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 col-sm-6 single">
+            <div class="product">
+                <a href="details.php">
+                    <img class="img-responsive" src="admin_area/product_images/product-1.jpg" alt="Product 1">
+                </a>
+                <div class="text">
+                    <h3>
+                        <a href="details.php">
+                            M-Dev Woman T-Shirt
+                        </a>
+                    </h3>
+                    <p class="price">$30</p>
+                    <p class="button">
+                        <a href="details.php" class="btn btn-default">View Details</a>
+                        <a href="details.php" class="btn btn-primary">
+                            <i class="fa fa-shopping-cart">
+                                Add To Cart
+                            </i>
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 col-sm-6 single">
+            <div class="product">
+                <a href="details.php">
+                    <img class="img-responsive" src="admin_area/product_images/product-2.jpg" alt="Product 1">
+                </a>
+                <div class="text">
+                    <h3>
+                        <a href="details.php">
+                            M-Dev Man Logo T-Shirt 
+                        </a>
+                    </h3>
+                    <p class="price">$30</p>
+                    <p class="button">
+                        <a href="details.php" class="btn btn-default">View Details</a>
+                        <a href="details.php" class="btn btn-primary">
+                            <i class="fa fa-shopping-cart">
+                                Add To Cart
+                            </i>
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
     <script src="js/jquery-331.min.js"></script>
     <script src="js/bootstrap-337.min.js"></script>
