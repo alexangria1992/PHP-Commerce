0d53d84db52f4fbf5be949550dc28dc6ed175d74
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-06-12 22:58:09 +1200

    Created Footer

---
+++ includes/footer.php
@@ -0,0 +1,86 @@
+<div id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-md-3">
+            <h4>Pages</h4>
+            <ul>
+            <li><a href="cart.php">Shopping Cart</a></li>
+            <li><a href="">Contact Us</a></li>
+            <li><a href="">Shop</a></li>
+            <li><a href="">My Accout</a></li>
+            </ul>
+            <hr>
+            <h4>User section</h4>
+            <ul>
+                <li><a href="checkout.php">Login</a></li>
+                <li><a href="checkout.php">Register</a></li>
+            </ul>
+            <hr class="hidden-md hidden-lg hidden-sm">
+
+            </div>
+            <div class="com-sm-6 col-md-3">
+                <h4>Top Products Categories</h4>
+                <ul>
+                    <li><a href="">Jackets</a></li>
+                    <li><a href="">Accessories</a></li>
+                    <li><a href="">Coats</a></li>
+                    <li><a href="">Shoes</a></li>
+                    <li><a href="">T-Shirts</a></li>
+
+                </ul>
+                <hr class="hidden-md hidden-lg">
+
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <h4>Find Us</h4>
+                <p>
+                    <strong>M-Dev Media inc.</strong>
+                    <br/>Cibubur
+                    <br/>Ciracas
+                    <br/>0818-0682-3157
+                    <br/><strong>MrGhie</strong>
+
+                </p>
+                <a href="contact.php">Check Our Contact Page</a>
+                <hr class="hidden-md hidden-lg">
+
+            </div>
+            <div class="col-sm-6 col-md-3">
+            <h4>Get The News</h4>
+            <p class="text-muted">
+            Don't miss our latest update products.
+            </p>
+            <form action="" method="post">
+                <div class="input-group">
+               <input type="text" class="form-control" name="email">
+               <span class="input-group-btn">
+                   <input type="submit" value="subscribe" class="btn btn-default">
+               </span>
+                </div>
+            </form>
+            <br>
+            <h4>Keep In Touch</h4>
+            <p class="social">
+                <a href="" class="fa fa-facebook"></a>
+                <a href="" class="fa fa-twitter"></a>
+                <a href="" class="fa fa-instagram"></a>
+                <a href="" class="fa fa-google-plus"></a>
+                <a href="" class="fa fa-envelope"></a>
+            </p>
+
+        </div>
+    </div>
+</div>
+
+<div class="copyright">
+    <div class="container">
+        <div class="col-sm-6">
+            <p class="pull-left">&copy; 2020 M-Dev Store All Rights Reserved</p>
+        </div>
+        <div class="col-md-6">
+            <p class="pull-right">
+                Theme by : <a href="">Alex Angrignon</a>
+            </p>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -371,6 +371,12 @@
     </div>
 </div>
 
+<?php
+
+include("includes/footer.php");
+
+?>
+
     <script src="js/jquery-331.min.js"></script>
     <script src="js/bootstrap-337.min.js"></script>
     
